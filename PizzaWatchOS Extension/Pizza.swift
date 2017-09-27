bec6a54d4964446ddf4440c15e1d3185252fb5b4
Juan Esteban Diaz <jdiaz@easysol.net>
2017-09-27 14:58:00 -0500

    Add Watch App Extension

---
+++ PizzaWatchOS Extension/Pizza.swift
@@ -0,0 +1,13 @@
+//
+//  Pizza.swift
+//  PizzaWatchOS Extension
+//
+//  Created by Juan Esteban Diaz on 26/09/17.
+//  Copyright © 2017 Juan Esteban Diaz. All rights reserved.
+//
+
+import WatchKit
+
+class Pizza: NSObject {
+
+}
